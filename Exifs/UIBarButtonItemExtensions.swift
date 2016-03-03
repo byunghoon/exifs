455974be3e208dabf3c8a5ac1d48786b4a33665b
Byunghoon Yoon <bh2yoon@gmail.com>
2016-03-02 22:15:18 -0500

    Add more methods in bar button item extensions

--- Exifs/UIBarButtonItemExtensions.swift
+++ Exifs/UIBarButtonItemExtensions.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 extension UIBarButtonItem {
+    class func flexibleItem() -> UIBarButtonItem {
+        return UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
+    }
+    
     class func spaceItem(width: CGFloat) -> UIBarButtonItem {
         let item = UIBarButtonItem(barButtonSystemItem: .FixedSpace, target: nil, action: nil)
         item.width = width
         return item
+    }
+    
+    convenience init(title: String?, target: AnyObject?, action: Selector) {
+        self.init(title: title, style: .Plain, target: target, action: action)
+        setTitleTextAttributes([NSForegroundColorAttributeName: Color.blue], forState: .Normal)
     }
 }
