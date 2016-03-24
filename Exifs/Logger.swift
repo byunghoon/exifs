31c13f57dbebfe99aad4748fa851ceba58def865
Byunghoon Yoon <bh2yoon@gmail.com>
2016-03-23 20:49:31 -0400

    Add placeholder logger

---
+++ Exifs/Logger.swift
@@ -0,0 +1,13 @@
+//
+//  Logger.swift
+//  Exifs
+//
+//  Created by Byunghoon Yoon on 2016-03-23.
+//  Copyright © 2016 Byunghoon. All rights reserved.
+//
+
+import Foundation
+
+func log(message: String) {
+    print(message)
+}
