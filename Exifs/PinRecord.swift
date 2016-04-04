41885deb2648b7df8fec552de408b33b41adf00c
Byunghoon Yoon <bh2yoon@gmail.com>
2016-04-04 08:15:42 -0400

    Add basic core data files

---
+++ Exifs/PinRecord.swift
@@ -0,0 +1,17 @@
+//
+//  PinRecord.swift
+//  Exifs
+//
+//  Created by Byunghoon Yoon on 2016-04-03.
+//  Copyright © 2016 Byunghoon. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class PinRecord: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
